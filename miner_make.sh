92c69ac94002bfeb5f6760f65e94783422a1db70
Libitina <libitinaxelloss@gmail.com>
2018-01-11 11:30:44 +0900

    dev20180111

---
+++ miner_make.sh
@@ -0,0 +1,9 @@
+cd deps
+bash ./build_win_deps.sh
+
+cd ..
+autoreconf -fi -I./deps/x86_64-w64-mingw32/share/aclocal
+./configure --host=x86_64-w64-mingw32 \
+CFLAGS="-Ofast -flto -march=native -I./deps/x86_64-w64-mingw32/include -std=c99 -DWIN32 -DCURL_STATICLIB -DPTW32_STATIC_LIB" \
+--with-libcurl=deps/x86_64-w64-mingw32 LDFLAGS="-L./deps/x86_64-w64-mingw32/lib -static"
+make
