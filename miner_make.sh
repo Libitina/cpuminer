bea0ddd92f8bbed3d6a63c2e716020d4cc4cd36f
Libitina <libitinaxelloss@gmail.com>
2018-01-31 15:40:38 +0900

    FIX CFLAGS

--- miner_make.sh
+++ miner_make.sh
@@ -8,9 +8,7 @@ cd ..
 
 autoreconf -fi -I./deps/x86_64-w64-mingw32/share/aclocal
 ./configure --host=x86_64-w64-mingw32 \
-CFLAGS="-Ofast -msse2 -msse4.1 -mfma4 -mavx -mavx2 -msha -flto -funroll-loops -fomit-frame-pointer -fprefetch-loop-arrays --param l1-cache-line-size=64 --param l1-c
-ache-size=32 --param l2-cache-size=512 -madx -mrdseed -mmwaitx -mxsavec -mxsaves -mclflushopt -mpopcnt -I./deps/x86_64-w64-mingw32/include -std=c99 -DWIN32 -DCURL_ST
-ATICLIB -DPTW32_STATIC_LIB" \
+CFLAGS="-Ofast -msse2 -msse4.1 -mfma4 -mavx -mavx2 -msha -flto -funroll-loops -fomit-frame-pointer -fprefetch-loop-arrays --param l1-cache-line-size=64 --param l1-cache-size=32 --param l2-cache-size=512 -madx -mrdseed -mmwaitx -mxsavec -mxsaves -mclflushopt -mpopcnt -I./deps/x86_64-w64-mingw32/include -std=c99 -DWIN32 -DCURL_STATICLIB -DPTW32_STATIC_LIB" \
 --enable-assembly \
 --with-libcurl=deps/x86_64-w64-mingw32 LDFLAGS="-L./deps/x86_64-w64-mingw32/lib -static"
 make clean
